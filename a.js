5e402cab3ed5beae6c717839500ab3bb3c185768
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2024-02-05 14:59:45 +0800

    Create a.js

---
+++ a.js
@@ -0,0 +1,10 @@
+
+  function handle(e){
+      if(e.keyCode === 13){
+        setUsername();
+      }
+  }  function CHAThandle(e){
+      if(e.keyCode === 13){
+        addMessage();
+      }
+  }
