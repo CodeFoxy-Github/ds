ffe91fd0ebfaca6766672ada6ffc438d3055cd88
CodeFoxy <96522178+CodeFoxy-Github@users.noreply.github.com>
2024-02-05 14:55:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5533,3 +5533,18 @@ async function upload(as) {
       `</a>`,
   );
 }
+function reseting() {
+  input.value = "";
+  document.getElementById("sdasd").click();
+}
+
+function handle(e) {
+  if (e.keyCode === 13) {
+    setUsername();
+  }
+}
+function CHAThandle(e) {
+  if (e.keyCode === 13) {
+    addMessage();
+  }
+}
